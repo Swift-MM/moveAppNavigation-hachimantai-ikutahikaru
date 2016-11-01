4fff76e02913831f783e2b999ba49d91906460a1
ikutahikaru <hikaruikuta@hanae-no-MacBook-Pro.local>
2016-11-01 13:18:17 +0900

    課題
    ３画面目までは行けたんですが、最初の画面に戻ると無限ループになってしまいます。

---
+++ Root View Controller.swift
@@ -0,0 +1,35 @@
+//
+//  Root View Controller.swift
+//  moveapp ver. Navi
+//
+//  Created by Hikaru Ikuta on 2016/10/31.
+//  Copyright © 2016年 Hikaru Ikuta. All rights reserved.
+//
+
+import UIKit
+
+class Root_View_Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
